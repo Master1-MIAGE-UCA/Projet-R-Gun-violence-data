fcaa2dc140fd069e830028f19d692f265e6f8aee
RemiFELIN <remi.felin@gmail.com>
2020-01-07 12:11:51 +0100

    Ajout code dans dossier final

---
+++ groupe-006/PRE/treatment_data.R
@@ -0,0 +1,115 @@
+# TRAITEMENT DES DONNEES
+#
+#
+
+# Import des librairies
+install.packages('lubridate')
+install.packages('xts')
+install.packages('zoo')
+install.packages('splitstackshape')
+install.packages('scales')
+install.packages('gridExtra')
+install.packages('stringr')
+install.packages('tibble')
+install.packages('ggplot2')
+install.packages('readr')
+install.packages('knitr')
+install.packages('stats')
+install.packages('graphics')
+install.packages('utils')
+install.packages('methods')
+install.packages('corrplot')
+install.packages('dplyr')
+
+library(knitr)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(tibble)
+library(stringr)
+library(gridExtra)
+library(scales)
+library(lubridate)
+library(ggrepel)
+library(leaflet)
+library(rgdal)
+library(tibble)
+library(purrr)
+library(splitstackshape)
+library(PerformanceAnalytics) 
+library(tidyr)
+library(corrplot)
+
+
+df = read.csv(file.choose(), header=TRUE,stringsAsFactors = FALSE, na.strings=c("NA", ""))
+df1 <- subset(df, select = -c(incident_url_fields_missing,location_description,notes,incident_url,incident_characteristics,longitude,latitude,sources, source_url))
+# - nombre  de blessés;
+# - nombre de morts;
+# - nombre de malfaiteurs;
+# - age des malfaiteurs;
+# - e ??tat dans lequel le fait divers a eu lieu. 
+
+
+#Valeurs manquantes
+apply(df1, 2, function(col)sum(is.na(col))/length(col))
+#We'll delete all the columns with more than 40% of missing values
+df1 <- subset(df1, select = -c(gun_stolen,gun_type,n_guns_involved,participant_name,participant_relationship))
+apply(df1, 2, function(col)sum(is.na(col))/length(col))
+#Basically, we should delete participant_age, because this column have 38.5% of missing data,
+#But we have to keep it, maybe we will use some data science methods to input missing data.
+
+#We delete all the missing values, we could replace it with Miss FAMD method, but as we aren't
+#doing Machine Learning, it's not necessary.
+
+df1=na.omit(df1)
+
+#
+#We'll change some data for the readibility of them
+age=cSplit(df1,c("participant_age"),sep="||",direction="wide",drop=FALSE)
+age$age=gsub(".*::","",age$participant_age)
+age$age=as.numeric(age$age)
+head(age$age)
+df1$participant_age=age$age
+
+type=cSplit(df1,"participant_status",sep="||" ,direction="wide",drop=FALSE)
+type$participant_status=gsub(".*::","",type$participant_status)
+head(type$participant_status)
+df1$participant_status=type$participant_status
+
+type=cSplit(df1,"participant_age_group",sep="||",direction="wide",drop=FALSE)
+type$participant_age_group=gsub(".*::","",type$participant_age_group)
+head(type$participant_age_group)
+df1$participant_age_group=type$participant_age_group
+
+type=cSplit(df1,c("participant_gender"),sep="||",direction="wide",drop=FALSE)
+type$participant_gender=gsub(".*::","",type$participant_gender)
+df1$participant_gender=type$participant_gender
+
+type=cSplit(df1,c("participant_type"),sep="||",direction="wide",drop=FALSE)
+type$participant_type=gsub(".*::","",type$participant_type)
+head(type$participant_type)
+df1$participant_type=type$participant_type
+
+
+#Now, after some preprocessing and the reorganization of the columns, we will kept only the
+#columns needed.
+#State, n_killed, n_injured, participant_ages
+df2 <- subset(df1, select = c(n_killed, n_injured, state, participant_age))
+df2
+
+#We delete the last NA
+df2=na.omit(df2)
+summary(df2$participant_age)
+#We can see a crazy value, we will delete it
+df2 <- df2[-which(df2$participant_age==209),]
+dfnum <- subset(df2, select = c(n_killed, n_injured, participant_age))
+
+#Now we can export data
+
+#choose your directory 
+setwd(choose.dir())
+
+#You can write .csv on your choosen directory
+write.csv(df2, "dataset_df2.csv")
+write.csv(df1, "dataset_df1.csv")
+write.csv(dfnum, "dataset_dfnum.csv")
---
+++ groupe-006/R/stat.R
@@ -0,0 +1,158 @@
+# STATISTIQUES ET TESTS D'HYPOTHESES
+#
+#
+
+# import libraries
+library(knitr)
+library(dplyr)
+library(readr)
+library(ggplot2)
+library(tibble)
+library(stringr)
+library(gridExtra)
+library(scales)
+library(lubridate)
+library(ggrepel)
+library(leaflet)
+library(rgdal)
+library(tibble)
+library(purrr)
+library(splitstackshape)
+library(PerformanceAnalytics) 
+library(tidyr)
+library(corrplot)
+library(lubridate)
+
+#Choose "dataset_df1.csv" : you can generate it with "treatment_data.R" on 'PRE' folder
+df1 = read.csv(file.choose(), header=TRUE,stringsAsFactors = FALSE, na.strings=c("NA", ""))
+
+#Prenons les datas df1 avec les colonnes utiles
+data <- subset(df1, select = c(n_killed, n_injured, state, participant_age, date))
+
+#et prenons les datas recueillis en 2018 (01-01-2018 -> 31-12-2018)
+data_2018 <- subset(data, data$date >= as.Date('2018-01-01') & data$date <= as.Date('2018-03-31'))
+
+#Pour la période suivante : 01-01-2013 -> 31-12-2017
+data_With_Periode <- subset(data, data$date >= as.Date('2013-01-01') & data$date <= as.Date('2017-12-31'))
+
+
+
+#Etudions plus en détail ce jeu de données
+summary(data_With_Periode)
+#a voir si on peut améliorer cette partie 
+
+#Considérons l'échantillon suivant : data_2018
+summary(data_2018)
+
+
+# Si nous comparons les résultats, nous remarquons les choses suivantes :
+
+# H0 NB MORT : Le nombre de morts obtenus en 2018 a permis de modifier la moyenne de mort 
+# HO NB BLESSE : Le nombre de blesses obtenus en 2018 a permis de modifier la moyenne de blesses 
+# HO AGE PARTICIPANT : L'age des participants releves en 2018 a permis de modifier la moyenne d'age 
+
+#
+# Pour les variables relatives aux nombres de mort, blesses et l'age des protagonistes : nous effectuerons
+# un test bilatérale afin de valider ou non H0
+#
+
+# Etablissons nos fonctions au préalable, elles faciliterons le traitement et ce sera plus lisible
+RejectRegion <- function(mu, n, xbar, sigma) {
+  res = abs(xbar-mu)/(sigma/sqrt(n))
+  return(res)
+}
+
+p_value <- function(alpha, ddl) {
+  pval = qt(alpha, df=ddl, lower.tail = FALSE)
+  return(pval)
+}
+
+sigma_value <- function(data) {
+  res = var(data)
+  return(res)
+}
+
+###################
+#                 #
+# DEBUT DES TESTS #
+#                 #
+###################
+
+#data fixes
+
+#taille de l'echantillon
+n <- length(data_2018)
+# On trouve notre valeur de comparaison (P-value) avec alpha = 5%
+tPVAL <- p_value(0.05, n-1)
+
+#############################################################
+
+#
+## Pour H0 NB DE MORTS
+#
+
+#d'après les resultats obtenus, on a :
+mu_NbMort <- mean(data_2018$n_killed)
+Xbar_NbMort <- mean(data_With_Periode$n_killed)
+sigma_NbMort <- sigma_value(data_2018$n_killed)
+
+# On applique notre test avec les données obtenues
+t_NbMort <- RejectRegion(mu_NbMort, n, Xbar_NbMort, sigma_NbMort)
+
+# On compare et on regarde le résultat 
+if(t_NbMort >= -tPVAL && t_NbMort <= tPVAL) {
+  cat("pour alpha = 5%, on a t_nbMort (", t_NbMort ,") appartenant à l'intervalle [", 
+      -tPVAL , ";", tPVAL, "]\n-> Ainsi, on ne peut pas refuser H0\n")
+} else {
+  cat("pour alpha = 5%, on a t_nbMort (", t_NbMort ,") n'appartenant pas à l'intervalle [", 
+      -tPVAL , ";", tPVAL, "]\n-> Ainsi,on rejette H0\n")
+}
+
+#############################################################
+
+#
+## Pour H0 NB DE BLESSES
+#
+
+#d'après les resultats obtenus, on a :
+mu_NbBlesses <- mean(data_2018$n_injured)
+Xbar_NbBlesses <- mean(data_With_Periode$n_injured)
+sigma_NbBlesses <- sigma_value(data_2018$n_injured)
+
+# On applique notre test avec les données obtenues
+t_NbBlesses <- RejectRegion(mu_NbBlesses, n, Xbar_NbBlesses, sigma_NbBlesses)
+
+# On compare et on regarde le résultat 
+if(t_NbBlesses >= -tPVAL && t_NbBlesses <= tPVAL) {
+  cat("pour alpha = 5%, on a t_nbMort (", t_NbBlesses ,") appartenant à l'intervalle [", 
+      -tPVAL , ";", tPVAL, "]\n-> Ainsi, on ne peut pas refuser H0\n")
+} else {
+  cat("pour alpha = 5%, on a t_nbMort (", t_NbBlesses ,") n'appartenant pas à l'intervalle [", 
+      -tPVAL , ";", tPVAL, "]\n-> Ainsi,on rejette H0\n")
+}
+
+#############################################################
+
+#
+## Pour l'age des participants
+#
+
+#d'après les resultats obtenus, on a :
+mu_Age <- mean(data_2018$participant_age)
+Xbar_Age <- 29.63 # mean(data_With_Periode$participant_age) la cmd ne marche pas ... à voir
+#tmp : on la saisie à la main
+sigma_Age <- sigma_value(data_2018$participant_age)
+
+# On applique notre test avec les données obtenues
+t_Age <- RejectRegion(mu_Age, n, Xbar_Age, sigma_Age)
+
+# On compare et on regarde le résultat 
+if(t_Age >= -tPVAL && t_Age <= tPVAL) {
+  cat("pour alpha = 5%, on a t_nbMort (", t_Age ,") appartenant à l'intervalle [", 
+      -tPVAL , ";", tPVAL, "]\n-> Ainsi, on ne peut pas refuser H0\n")
+} else {
+  cat("pour alpha = 5%, on a t_nbMort (", t_Age ,") n'appartenant pas à l'intervalle [", 
+      -tPVAL , ";", tPVAL, "]\n-> Ainsi,on rejette H0\n")
+}
+
+#############################################################
